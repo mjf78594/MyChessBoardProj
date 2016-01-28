30678569184931af4f74d9498fe2dcf02568fe06
mjf78594 <mjf78594@bethel.edu>
2016-01-28 16:58:13 -0600

    Worked on color array for the table

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-echo "Do stuff";
+<?php
+require_once '/Users/mjf78594/Documents/MyChessBoardProj/vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('/Users/mjf78594/Documents/MyChessBoardProj/twig/');
+$twig = new Twig_Environment($loader);
+$color_array = array(for($i =0; $i < 8; $i++){
+  array(for($j = 0; $j < 8; $j++){
+    if($i % 2 === 0){
+      if($j %2 === 0){
+        <td bgcolor="Black"></td>
+      }
+    }elseif ($i % 2 != 0) {
+      if($j % 2 === 0) {
+        <td bgcolor="White"></td>
+      }
+    }elseif ($j % 2 === 0) {
+      <td bgcolor="Black"></td>
+    }else {
+      <td bgcolor="White"></td>
+    }
+  })
+});
+$html = $twig->render('board.html', array(
+	'color_array' => $color_array,
+  "test" => "hello world"
+	));
+echo $html;
+?>
