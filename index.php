12291e0e435b91fb636b0ce2ae366d598dbd4d23
mjf78594 <mjf78594@bethel.edu>
2016-01-29 13:18:22 -0600

    Finished the board, iterators needed to be used independently

--- index.php
+++ index.php
@@ -6,22 +6,21 @@ for($i = 0; $i < 8; $i++){
   for($j = 0; $j <8; $j++){
     if($i % 2 == 0){
       if($j % 2 == 0){
-        $color_array[$i][$j] = 'White';
+        $color_array[$i][$j] = 0;
       }else{
-        $color_array[$i][$j] = 'Black';
+        $color_array[$i][$j] = 1;
       }
     }else{
       if($j % 2 == 0){
-        $color_array[$i][$j] = 'Black';
+        $color_array[$i][$j] = 1;
       }else{
-        $color_array[$i][$j] = 'White';
+        $color_array[$i][$j] = 0;
       }
     }
   }
 }
 $html = $twig->render('board.html', array(
-	'color_array' => $color_array,
-  "test" => "hello world"
+	'color_array' => $color_array
 	));
 echo $html;
 ?>
