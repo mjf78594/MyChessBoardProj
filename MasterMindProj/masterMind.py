1fc0a20d5be3cc14934e87f40b0e69d7b04d2625
mjf78594 <mjf78594@bethel.edu>
2016-02-11 10:45:51 -0600

    Corrected syntax errors using pycharm. Also realized that I need to implement a makeFourDigit method to make sure that the guesses and actual value are set as a four digit number i.e. 0000 instead of just 0

--- MasterMindProj/masterMind.py
+++ MasterMindProj/masterMind.py
@@ -1,65 +1,65 @@
 from random import randint
 
 def generateNum():
-    actual = randint(0000, 9999);
-    return actual;
+    actual = randint(0000, 9999)
+    return actual
 
 def getGuess():
-    guess = -1;
+    guess = -1
     while guess < 0 and guess >= 10000:
-        guess = raw_input("Guess a number betweeen 0000 and 9999:"):
-    return guess;
+        guess = raw_input("Guess a number betweeen 0000 and 9999:")
+    return guess
 
 def main():
-    actual = generateNum();
-    actual_Array = convertToArray(actual);
-    guess = getGuess();
-    guess_Array = convertToArray(guess);
-    turns = 1;
+    actual = generateNum()
+    actual_Array = convertToArray(actual)
+    guess = getGuess()
+    guess_Array = convertToArray(guess)
+    turns = 1
     while actual != getGuess:
-        cows_Arr = checkCows(actual_Array, guess_Array);
-        bulls = checkBulls(actual_Array, guess_Array, cows_Arr);
-        cows = count(cows_Arr);
-        print "Your guess generated " cows "cows and " bulls "bulls.";
-        guess = getGuess();
-        guess_Array = convertToArray(guess);
+        cows_Arr = checkCows(actual_Array, guess_Array)
+        bulls = checkBulls(actual_Array, guess_Array, cows_Arr)
+        cows = countCows(cows_Arr)
+        print "Your guess generated " + cows + "cows and " + bulls + "bulls."
+        guess = getGuess()
+        guess_Array = convertToArray(guess)
         turns = turns + 1;
-    print "You guessed it! And it only too you " turns "!";
+    print "You guessed it! And it only too you " + turns + "!"
 
 def convertToArray(num):
-    convert = str(num);
-    arr = [];
-    for digit in num:
-        arr.append(int(digit));
-    return arr;
+    convert = str(num)
+    arr = []
+    for digit in convert:
+        arr.append(digit)
+    return arr
 
 def countCows(arr):
-    i = 0;
+    i = 0
     for binum in arr:
         if arr[binum] == 1:
-            i = i + 1;
-    return i;
+            i = i + 1
+    return i
 
 def checkCows(actual, guess):
-    cows = [];
+    cows = []
     for digit in range(len(actual)):
         if actual[digit] == guess[digit]:
-            cows.append(1);
+            cows.append(1)
         else:
-            cows.append(0);
-    return cows;
+            cows.append(0)
+    return cows
 
 def checkBulls(actual, guess, cows):
-    bulls = 0;
+    bulls = 0
     for digit in range(len(actual)):
         if cows[digit] == 1:
-            continue;
+            continue
         elif guess[digit] in actual:
-            bulls = bulls + 1;
-    cow = countCows(cows);
+            bulls = bulls + 1
+    cow = countCows(cows)
     if cow + bulls > 4:
-        bulls = bulls - cow;
-main();
+        bulls = bulls - cow
+main()
 
 
 
