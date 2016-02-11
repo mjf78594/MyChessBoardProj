dc65c8bd17c14862c392f748a47bbdeecfc753b9
mjf78594 <mjf78594@bethel.edu>
2016-02-11 09:17:32 -0600

    Created a main method to run the game

--- MasterMindProj/masterMind.py
+++ MasterMindProj/masterMind.py
@@ -7,3 +7,11 @@ def getGuess():
     while guess < 0 and guess >= 10000:
         guess = raw_input("Guess a number betweeen 0000 and 9999:"):
     return guess;
+def main():
+    actual = generateNum();
+    guess = getGuess();
+    while actual != getGuess:
+        cows = checkCows(actual, guess);
+        bulls = checkBulls(actual, guess);
+        print "Your guess generated " "cows and " bulls "bulls.";
+        guess = getGuess();
