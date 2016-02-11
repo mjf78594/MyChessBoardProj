69fe1d411184e123a7e9d8623b918e0fe20fff16
mjf78594 <mjf78594@bethel.edu>
2016-02-11 09:10:32 -0600

    Created a getGuess method which asks  the user for a guess

--- MasterMindProj/masterMind.py
+++ MasterMindProj/masterMind.py
@@ -1,4 +1,9 @@
 from random import randint
 def generateNum():
-    actual = randint(0000, 9999)
+    actual = randint(0000, 9999);
     return actual;
+def getGuess():
+    guess = -1;
+    while guess < 0 and guess >= 10000:
+        guess = raw_input("Guess a number betweeen 0000 and 9999:"):
+    return guess;
